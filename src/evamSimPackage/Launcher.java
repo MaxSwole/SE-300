310f89b1955cbd96613eab9d806b627e69603143
MaxSwole <maxwellkappel@gmail.com>
2020-09-22 20:40:25 -0400

    RESTARTING AHHHHHH

--- src/evamSimPackage/Launcher.java
+++
@@ -1,9 +0,0 @@
-package evamSimPackage;
-
-public class Launcher {
-    public static void main(String[] args){
-        //Starting the Sim,
-        Sim sim = new Sim("Evac Simulator", 400, 400);
-        sim.start();
-    }
-}
