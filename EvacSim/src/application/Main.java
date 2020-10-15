af16f09dec57c491ef01af9e685ada79c225c0d0
Miguel <Miguel@DESKTOP-6F5NAMA>
2020-10-14 20:56:49 -0400

    added comment

--- EvacSim/src/application/Main.java
+++ EvacSim/src/application/Main.java
@@ -8,7 +8,7 @@ public class Main extends Application {
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-
+		//THIS IS MIGUEL
 		ViewManager manager = new ViewManager();
 		primaryStage = manager.getMainStage();
 		primaryStage.show();
