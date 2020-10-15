e84afb419283c9c6780dc20c9ae5bf811f651296
Owner <Owner@DESKTOP-R0T16CP.attlocal.net>
2020-10-14 21:18:24 -0400

    miles 3

--- EvacSim/src/application/Main.java
+++ EvacSim/src/application/Main.java
@@ -9,7 +9,7 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 		//THIS IS MIGUEL
-		//this is miles 2
+		//this is miles 3
 		ViewManager manager = new ViewManager();
 		primaryStage = manager.getMainStage();
 		primaryStage.show();
