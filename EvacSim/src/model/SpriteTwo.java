c1f3f90549e300e4bebd9a794dc83c0300f9a61e
Maxwell Kappel <maxwellkappel@gmail.com>
2020-10-27 11:25:14 -0400

    Adding new Sprite class to be fiddled with

---
+++ EvacSim/src/model/SpriteTwo.java
@@ -0,0 +1,15 @@
+package model;
+
+import javafx.geometry.Point2D;
+
+public class SpriteTwo {
+
+	private Point2D location;
+	private Point2D velocity;
+	private Point2D acceleration;
+	
+	public void move() {
+	
+	}
+	
+}
