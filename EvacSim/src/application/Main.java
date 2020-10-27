c3ac24641c9516751a583ca77b50f62d7bc2c99e
Maxwell Kappel <maxwellkappel@gmail.com>
2020-10-27 11:05:29 -0400

    Added Program Icon

--- EvacSim/src/application/Main.java
+++ EvacSim/src/application/Main.java
@@ -1,6 +1,7 @@
 package application;
 
 import javafx.application.Application;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 import view.ViewManager;
 
@@ -10,6 +11,7 @@ public class Main extends Application {
 	public void start(Stage primaryStage) throws Exception {
 		ViewManager manager = new ViewManager();
 		primaryStage = manager.getMainStage();
+		primaryStage.getIcons().add(new Image("view/resources/Icon.png"));
 		primaryStage.show();
 	}
 	
